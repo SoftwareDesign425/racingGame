fbbeadeb13ef6f36ca97d6249ee3f4eeabae733d
joe256 <j_mitchell22@post.massbay.edu>
2018-03-07 18:46:07 -0500

    Added tracks to the scene

--- RacingGame/src/Animation.java
+++ RacingGame/src/Animation.java
@@ -1,39 +1,58 @@
-
-import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 /*
 Joseph Mitchell
 
 This class handles animations like, tracks, cars, ect.
+
+This class has the following attributes:
+
+
+track(Image) - this is the image of the track(may later be collection)
 
 Purpose:
 To split the gui so that the gui is not too long.
 */
 public class Animation extends Scene{
     
-    private Scene root;
-    private Group layout;
+    private Image track;
+    private int wid, heigh;
     
     public Animation(Parent root1) {
-        super(root1, 2000, 2000);
-        Pane p = new Pane();
-        ((Group)getRoot()).getChildren().add(p);
+        super(root1, 500, 500);
+        track = new Image("https://docs.telerik.com/devtools/winforms/telerik-presentation-framework/shapes/images/donut-shape001.png");
+        wid = 0;heigh = 0;//initialize width and height of game to 0.
+        ImageView iv = new ImageView();
+        iv.setImage(track);
+        iv.setSmooth(true);
+        setGameDim(iv);
     }
     
-//    public void start(Stage primaryStage) throws Exception {
-//        root = new Scene(layout, 2000, 2000);
-//        primaryStage.setScene(root);
-//        primaryStage.show();
-//    
-//    }
+    public void setGameDim(ImageView i){
+        String info = "", width = "", height = "";
+        info += i.boundsInParentProperty();
+        width = info.substring(info.indexOf("width:"), info.indexOf(",",info.indexOf("width:")));
+        height = info.substring(info.indexOf("height:"), info.indexOf(",",info.indexOf("height:")));
+        
+        width = width.substring(width.indexOf(":")+1, width.indexOf(".", width.indexOf(":")));
+        height = height.substring(height.indexOf(":")+1, height.indexOf(".", height.indexOf(":")));
+        
+        wid = Integer.parseInt(width);
+        heigh = Integer.parseInt(height);
+        
+        System.out.println("Width: " + wid + "\nHeight: " + heigh);
+        
+        ((Group)getRoot()).getChildren().add(i);
+    }
     
-//    public static void main(String[] args){
-//        launch(args);
-//    }
+    public int getGameWidth(){
+        return wid;
+    }
     
+    public int getGameHeight(){
+        return heigh;
+    }
 }
