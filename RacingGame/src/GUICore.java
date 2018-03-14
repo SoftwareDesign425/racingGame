3fc357212095a447bd11c765709a55751de84e0f
Michael Swann <m_swann@massbay.edu>
2018-03-14 14:15:56 -0400

    Added buttons and help prompt, compacted layout slightly

--- RacingGame/src/GUICore.java
+++ RacingGame/src/GUICore.java
@@ -5,8 +5,8 @@
  */
 
 /* TO-DO:
- * ADD BUTTONS
- * ADD PLUG FOR ANIMATION CLASS
+ * FINISH BUTTON FUNCTIONS
+ * TWEAK SPACINGS
  * ADD FILE I/O
  */
 
@@ -17,42 +17,102 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 // Layout management
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+// Menu things (buttons, mouse input, etc)
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent; 
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+// Alerts/Pop-Ups
+import java.util.Optional;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 // Class Compatibility
 import javafx.scene.Parent;
 
 public class GUICore extends Application{
   
+  // Variables
+  private String inputString;
+  private Alert helpAlert;
+  
   public void start(Stage stage){
     
 //******************* Set-Up *******************//
     
+    // Main body
+    inputString = "../inputFile.txt"; // Initialize this String
     Venue coreVenue = new Venue();
-    coreVenue.load("inputFile.txt");
+    coreVenue.load(inputString);
     Animation coreAnim = new Animation(coreVenue);
     Pane corePane = new Pane();
-    corePane.setPrefSize(800,500); // Allotting a 300x500 space for our menu
+    corePane.setPrefSize(625,500); // Allotting a 125x500 space for our menu
     
-
-    // Add buttons
-    //gp.add(guiButtons, 0,0);
-    // Add animation
-    //gp.add(animation, 0,0);
+    // Alerts
+    helpAlert = new Alert(AlertType.INFORMATION, "Help information will go here once created.");
+    helpAlert.setHeaderText("Game Help");
+    
+//******************* Menu *******************//
+    
+    // Start button
+    Button startButton = new Button("Begin Race");
+    startButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event){
+        coreAnim.render(); // Not implemented properly yet
+      }
+    }));
+    startButton.relocate(525, 25);
+    
+    // Reset button
+    Button resetButton = new Button("Reset Race");
+    resetButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event){
+        // No reset method exists yet
+      }
+    }));
+    resetButton.relocate(525, 55);
+    
+    // Select File button
+    Button fileButton = new Button("Select Race");
+    fileButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event){
+        fileSelection(); // Not implemented yet
+      }
+    }));
+    fileButton.relocate(525, 85);
+    
+    // Help button
+     Button helpButton = new Button("Help");
+    helpButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event){
+        helpAlert.show(); // Display our help alert
+      }
+    }));
+    helpButton.relocate(525, 115);
+    
+    // Button Grouping
+    Group buttons = new Group(startButton, resetButton, fileButton, helpButton);
+    corePane.getChildren().add(buttons);
+    
 //******************* Animation *******************//
     
     Pane animPane = coreAnim.createContent(); // This is a 500x500 Pane
     corePane.getChildren().add(animPane);
     animPane.relocate(0,0); // Move animation pane to top left corner
+    
 //******************* Display *******************//
     
     Scene menu = new Scene(corePane);
-    //Animation animator = new Animation(p);
     stage.setTitle("Project 3 - Racing Game");       
     stage.setScene(menu); 
     stage.setResizable(true);    
     stage.sizeToScene();
     stage.show();
     
+  }
+  
+  // File selection
+  public void fileSelection(){
   }
   
   // Run program
